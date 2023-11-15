831bf1c669739aaf6cf86f5a44bfea92182de983
Zachary Smith <22395357+Cutwell@users.noreply.github.com>
2023-11-15 14:38:43 +0000

    Made tests names dynamic

--- api/packages/pirate-speak/tests/test_chain.py
+++ api/packages/pirate-speak/tests/test_chain.py
@@ -1,12 +1,13 @@
-import langsmith
 from pirate_speak.chain import chain
+from datetime import datetime
+import langsmith
 
 def test_chain():
 	client = langsmith.Client()
 	chain_results = client.run_on_dataset(
 		dataset_name="quickstart-dataset",
 		llm_or_chain_factory=chain,
-		project_name="test-fixed-vibration-74",
+		project_name=f"quickstart-dataset-test-{int(datetime.now().strftime('%Y%m%d%H%M%S'))}",
 		concurrency_level=5,
 		verbose=True,
 	)
